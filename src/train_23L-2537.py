86be17dd8587d800bffe5c3fa04f603c0780a617
Maheer Khurram <maheer.khurram1717@gmail.com>
2026-09-10 23:03:27 -0700

    Add feature scaling technique - 23L-2537

--- src/train_23L-2537.py
+++ src/train_23L-2537.py
@@ -18,7 +18,7 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     n_estimators = 100
-    learning_rate=0.05
+    learning_rate=0.05 # feature scaling: apply MinMaxScaler instead of standardization
 
     model = RandomForestRegressor(n_estimators=n_estimators, random_state=42)
     model.fit(X_train, y_train)
